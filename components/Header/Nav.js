31b8d22bf51995b87929a80c3bfc6d56fbedd2b8
thangtm2 <thangtm2@vng.com.vn>
2021-12-06 22:02:40 +0700

    feat: update Header responsive

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -2,6 +2,13 @@ import Link from "next/link";
 import { useRouter } from "next/router"
 import styles from "./Header.module.scss";
 import className from "utils/className";
+import {
+  Stack,
+  Text,
+  IconButton,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+ import { FiSun } from "react-icons/fi";
 
 const NavItems = [{
   label: "Posts",
@@ -11,22 +18,56 @@ const NavItems = [{
   path: "/about",
 }]
 
-function Nav() {
+function Nav({ toggleOn }) {
   const { pathname } = useRouter();
+  const toggleVisible = useBreakpointValue({ base: true, md: false });
+  const iconColor = useBreakpointValue({ base: "white", md: "gray" });
+  const textColor = useBreakpointValue({ base: "white", md: "#212121" });
+
+  if (toggleVisible && !toggleOn) {
+    return null;
+  }
   return (
-    <nav>
-      <ul>
+    <Stack
+      spacing={8}
+      flex={1}
+      justify={{ base: "flex-start", md: "space-between" }}
+      direction={{ base: "column", md: "row" }}
+      className={className(toggleVisible && styles.navMobile)}
+      pl={{ base: 0, md: 10 }}
+      pb={{ base: 12, md: 0}}
+    >
+      <Stack
+        direction={{ base: "column", md: "row" }}
+      >
         {NavItems.map(({ label, path }) => {
+          const selected = path === pathname
           return (
-            <li key={path} className={className(path === pathname && styles.selected)}>
-              <Link href={path} passHref>
-              {label}
+            <Link key={label} href={path} passHref>
+              <Text
+                className={className(path === pathname && styles.selected)}
+                color={textColor}
+                padding={{ base: 0, md: 2 }}
+                fontSize={{ base: "2xl", md: "lg"}}
+                fontWeight={selected ? 700 : 600}
+                borderBottom={selected && !toggleVisible && "2px solid #ff4081"}
+              >
+                {label}
+              </Text>    
             </Link>
-            </li>
           );
         })}
-      </ul>
-    </nav>
+      </Stack>
+      <Stack
+        direction={"row"}
+      >
+        <IconButton
+          size={25}
+          colorScheme={iconColor}
+          icon={<FiSun size={25} />}
+        />
+      </Stack>
+    </Stack>
   )
 }
 
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Link from "next/link";
 import Nav from "./Nav";
+import MenuToggle from "./MenuToggle";
 import styles from "./Header.module.scss";
 
 function Header() {
+  const [toggleOn, setToggleOn] = useState(false);
+  function handleMenuToggle() {
+    setToggleOn(on => !on);
+  }
   return (
     <header className={styles.header}>
-      <Link href="/" passHref>
-        <h1 className={styles.name}>Es kommst</h1>
-      </Link>
-      <Nav />
+      <h1 className={styles.name}>
+        <Link href="/" passHref>Es kommst</Link>
+      </h1>
+      <Nav toggleOn={toggleOn} />
+      <MenuToggle onToggle={handleMenuToggle} toggleOn={toggleOn}/>
     </header>
   );
 }
