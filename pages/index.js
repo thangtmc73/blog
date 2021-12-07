ff610295243debea4c1c51c6598b5372186726f8
thangtm2 <thangtm2@vng.com.vn>
2021-12-07 11:26:42 +0700

    feat: apply dark mode for home page

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,21 @@ import Head from "next/head";
 import Header from "components/Header";
 import styles from "styles/Layout.module.scss";
 import className from "utils/className";
-import { Box } from "@chakra-ui/react";
-
+import { ThemeBox } from "components/ThemeComponent";
+import ThemeConfig from "theme/theme-config";
 export default function Home() {
   return (
     <div className={styles.page}>
       <Head>
         <title>Posts</title>
       </Head>
-      <Box
-        backgroundColor="#f4f5f6"
+      <ThemeBox
+        bgColorConfig={ThemeConfig.defaultHeaderBackgroundColor}
       >
         <div className={className(styles.maxWidthWrapper)}>
           <Header />
         </div>
-      </Box>
+      </ThemeBox>
       <div className={className(styles.maxWidthWrapper, styles.mainContent)}>
       </div>
     </div>
