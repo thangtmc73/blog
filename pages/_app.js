4053d23e3e35508e3cb662a37d5350d747c3167e
thangtm2 <thangtm2@vng.com.vn>
2021-12-09 17:31:54 +0700

    feat: apply next-i18next to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { appWithTranslation } from 'next-i18next'
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import Header from "components/Header"
@@ -12,4 +13,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default appWithTranslation(MyApp);
