984fe3ae90741302cc0722f9111ee9966637a13a
thangtm2 <thangtm2@vng.com.vn>
2021-12-09 17:30:58 +0700

    feat: add locales config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ["en", "vi"],
     defaultLocale: "en",
     localeDetection: true,
   },
+  localePath: path.resolve("./locales"),
 };
