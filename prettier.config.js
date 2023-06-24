a5a42537aeaaedc0e060bc6f8662058dae089393
thangtm2 <thangtm2@vng.com.vn>
2023-06-25 00:46:59 +0700

    feat: initial install tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,11 @@
 const options = {
+  plugins: [require('prettier-plugin-tailwindcss')],
   arrowParens: "always",
   singleQuote: false,
   bracketSpacing: true,
   semi: true,
   tabWidth: 2,
-  trailingComma: 'es5'
+  trailingComma: 'es5',
 }
 
 module.exports = options
