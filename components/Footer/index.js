8964726e04c062c1ccbe4269c85065fdf61f8f85
thangtm2 <thangtm2@vng.com.vn>
2023-06-25 19:25:49 +0700

    feat: update footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,38 +4,28 @@ import {
   Grid,
 } from "@chakra-ui/react";
 import SocialContacts from "./SocialContacts";
-import styles from "./Footer.module.scss";
-import { ThemeBox, ThemeText } from "components/ThemeComponent";
-import ThemeConfig from "theme/theme-config";
 
 function Footer() {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <ThemeBox bgColorConfig={ThemeConfig.defaultFooterBackgroundColor}>
-      <footer className={styles.footer}>
-        <Grid
-          w={"100%"}
-          gridTemplateColumns={{ base: "repeat(1, 1fr)", md: "1fr 300px"}}
-        >
-          <div>
-            <ThemeText
-              className={styles.name}
-              textAlign={{ base: "center", md: "left"}}
-            >
-              <Link href="/" passHref>Es kommst</Link>
-            </ThemeText>
-            {!isMobile && <ThemeText fontSize="md">Cảm ơn bạn đã ghé qua</ThemeText>}
-          </div>
-          <SocialContacts />
-          <ThemeText
-            textAlign={{ base: "center", md: "left"}}
-          >
-            2021-present Thang Minh Tran
-          </ThemeText>
-        </Grid>
-      </footer>
-    </ThemeBox>
+    <footer className="flex w-full max-w-screen-lg mx-auto py-6 px-4">
+      <Grid
+        w={"100%"}
+        gridTemplateColumns={{ base: "repeat(1, 1fr)", md: "1fr 300px" }}
+      >
+        <div>
+          <p className="text-center md:text-left text-2xl text-pink dark:text-pink-d font-bold">
+            <Link href="/" passHref>Es kommst</Link>
+          </p>
+          {!isMobile && <p className="text-md">Cảm ơn bạn đã ghé qua</p>}
+        </div>
+        <SocialContacts />
+        <p className="text-center md:text-left">
+          2021-present Thang Minh Tran
+        </p>
+      </Grid>
+    </footer>
   );
 }
 
