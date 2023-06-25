e58c7f3806af4017dc2c7a990e2c9ccc631ba21f
thangtm2 <thangtm2@vng.com.vn>
2023-06-25 20:51:48 +0700

    feat: add useHasMounted to fix expected server HTML to contain

--- components/Header/DarkModeButton.js
+++ components/Header/DarkModeButton.js
@@ -1,9 +1,12 @@
 import { FiSun, FiMoon } from "react-icons/fi";
 import { useContext } from "react";
 import ThemeContext from "theme/context";
+import useHasMounted from "hooks/useHasMounted";
 
 function DarkModeButton() {
   const { colorMode, toggleColorMode } = useContext(ThemeContext);
+  const hasMounted = useHasMounted();
+  if (!hasMounted) return null;
   return (
     <button className="text-default-fg dark:text-default-fg-d" onClick={toggleColorMode}>
       {colorMode === "light"
