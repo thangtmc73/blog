285e6a8d0f13f62f3d2c3801e46701ab081c05fc
thangtm2 <thangtm2@vng.com.vn>
2023-06-25 21:48:10 +0700

    feat: update SocialContacts

--- components/Footer/SocialContacts.js
+++ components/Footer/SocialContacts.js
@@ -1,54 +1,35 @@
 import Link from "next/link";
-import {
-  Stack,
-  Box,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import ThemeConfig from "theme/theme-config";
 import { FiGithub, FiMail } from "react-icons/fi";
 
 function SocialContacts() {
-  const iconColor = useColorModeValue(...ThemeConfig.primaryText);
-
   return (
-    <Stack
-      direction={"row"}
-      pt={5}
-      pb={5}
-      justifyContent={{ base: "center", md: "flex-end" }}
-      spacing={4}
-    >
+    <div className="flex py-1 gap-2 justify-center md:justify-end">
       <SocialItem
         href={"https://github.com/thangtmc73"}
-        color={iconColor}
         Component={FiGithub}
       />
       <SocialItem
         href="mailto:thangtmc73@gmail.com"
-        color={iconColor}
         Component={FiMail}
       />
       {/* <SocialItem
         color={iconColor}
         Component={FiCoffee}
       /> */}
-    </Stack>
+    </div>
   )
 }
 
-function SocialItem({ Component, color, href = "/" }) {
+function SocialItem({ Component, href = "/" }) {
   if (!Component) return null;
   return (
     <Link
       href={href}
       passHref
     >
-    <Box
-      padding={3}
-      borderRadius={24}
-    > 
-      <Component color={color} size={24} />
-    </Box>
+      <div className="rounded-full p-3 text-default-fg dark:text-default-fg-d">
+        <Component size={24} />
+      </div>
     </Link>
   )
 }
