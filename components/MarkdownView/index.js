3475a1dfd79f65f4225c79cec1d19d239a63f64b
thangtm2 <thangtm2@vng.com.vn>
2023-06-27 02:21:52 +0700

    feat: update markdown display

--- components/MarkdownView/index.js
+++ components/MarkdownView/index.js
@@ -6,24 +6,24 @@ import ThemeContext from "theme/context";
 import useHasMounted from "hooks/useHasMounted";
 
 const CodeBlock = (theme) => ({
-  h1: (props) => <h1 className="text-pink dark:text-pink-d my-2 text-3xl font-bold" {...props} />,
-  h2: (props) => <h2 className="text-green dark:text-green-d my-2 text-2xl font-bold" {...props} />,
+  h1: (props) => <h1 className="text-green dark:text-green-d my-2 text-3xl font-bold" {...props} />,
+  h2: (props) => <h2 className="text-purple dark:text-purple-d my-2 text-2xl font-bold" {...props} />,
   h3: (props) => <h3 className="text-orange dark:text-orange-d my-2 text-2xl font-semibold" {...props} />,
   h4: (props) => <h4 className="text-cyan dark:text-cyan-d my-2 text-xl font-semibold" {...props} />,
   h5: (props) => <h5 className="text-yellow dark:text-yellow-d my-2 text-xl" {...props} />,
   h6: (props) => <h6 className="text-yellow dark:text-yellow-d my-2" {...props} />,
   p: (props) => <p className="text-default-fg dark:text-default-fg-d text-md my-2" {...props} />,
   // eslint-disable-next-line @next/next/no-img-element
-  img: (props) => <img className="mx-auto" alt="img" {...props}/>,
-  a: (props) => <a className="p-1 text-purple dark:text-purple-d hover:border-b-2	hover:border-b-purple dark:hover:border-b-purple-d" {...props} />,
+  img: (props) => <img className="mx-auto" alt="img" {...props} />,
+  a: (props) => <a className="p-1 text-red dark:text-red-d hover:border-b-2 red:border-b-purple dark:hover:border-b-purple-d" {...props} />,
   code: ({ inline, className, children, ...props }) => {
     const match = /language-(\w+)/.exec(className || "")
     return !inline && match ? (
-      <SyntaxHighlighter 
+      <SyntaxHighlighter
         style={theme === "light" ? oneLight : dracula}
         className="w-full overflow-x-auto my-2"
         showLineNumbers={true}
-        language={match[1]} 
+        language={match[1]}
         PreTag="div"
         {...props}
       >
