f7e483c7ba150cc1716b423cd7d34553488de72e
thangtm2 <thangtm2@vng.com.vn>
2023-07-08 23:07:48 +0700

    feat: apply next fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import { appWithTranslation } from 'next-i18next'
 import "../styles/globals.css";
 import Header from "components/Header";
 import Footer from "components/Footer";
-import ThemeProvider from "theme/ThemeProvider"
+import ThemeProvider from "theme/ThemeProvider";
+import { Be_Vietnam_Pro } from "next/font/google";
+
+const be = Be_Vietnam_Pro({ weight: ["400", "600", "700"], subsets: ["vietnamese"] })
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      <div className="flex flex-col h-screen overflow-auto">
+      <div className={`flex flex-col h-screen overflow-auto ${be.className}`}>
       <Header />
         <div className="flex-1">
           <Component {...pageProps} />
